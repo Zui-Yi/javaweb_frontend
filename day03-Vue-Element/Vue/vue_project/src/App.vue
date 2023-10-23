578a7dd6d2a7e63dd3fa7174b8a960dc444683ac
荣翔 <2510060278@qq.com>
2023-10-24 03:51:17 +0800

    first commit

---
+++ day03-Vue-Element/Vue/vue_project/src/App.vue
@@ -0,0 +1,29 @@
+<template><!-- html代码 ，视图部分-->
+  <div>
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <element-view></element-view> -->
+
+    <!-- 员工管理 -->
+    <emp-view></emp-view>
+  </div>
+</template>
+
+
+<script> /* js代码 */
+import EmpView from './views/tlias/EmpView.vue'
+/* import ElementView from './views/element/ElementView.vue' */
+export default {
+  components:{EmpView/* ElementViewEmpView */},
+  data() {
+    return {
+      message: "hello world",
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style>/* css样式 */
+
+</style>
